f6e4d7d5bd30332005afaa4c6eef6917593d3330
pabloaugussto <augustopablo99@gmail.com>
2025-03-15 14:13:04 -0300

    Alterações nos arquivos vazios.

--- routes.py
+++ routes.py
@@ -1,1 +1,10 @@
- 
+from fastapi import APIRouter
+from .schemas import Investimento
+
+router = APIRouter()
+
+# Rota para listar investimentos
+@router.get("/investimentos")
+async def get_investimentos():
+    return {"message": "Aqui estarão os investimentos"}
+
